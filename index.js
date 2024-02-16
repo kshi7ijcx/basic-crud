f1fa2964f95485e920c221df301e0f6a677d77fc
Kshitij Chauhan <kshitij.cx@gmail.com>
2024-02-16 18:11:38 +0530

    get and post api added

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 import express from 'express'
 import mongoose from 'mongoose'
-import model from './models/products.model.js'
+import Product from './models/products.model.js'
 const app=express()
+
+app.use(express.json()) //allows passing request in form of json json
 
 app.get("/",(req,res)=>{
     res.send("Hello")
+})
+
+app.get("/api/products",async (req,res)=>{
+    try{
+        const product = await Product.find({})
+        res.status(200).json(product)
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.get("/api/products/:id", async (req,res)=>{
+    
+})
+
+app.post("/api/products", async (req,res)=>{
+    try{
+        const product = await Product.create(req.body);
+        res.status(200).json(product)
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
 })
 
 mongoose.connect("mongodb+srv://kshitijcx:rsGj66U4wZwsrXkA@backenddb.5ogbiju.mongodb.net/?retryWrites=true&w=majority").then(()=>{
